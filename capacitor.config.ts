15091fed7aca15d258ae326cf3521ca0a299245c
Nadim Shakil <nadim@iamsheba.xyz>
2021-06-13 21:46:31 +0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'trip-calculator',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
